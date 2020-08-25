bb60257eda78762048e13d886fa048ad7b92abcf
dd <211@qq.com>
2020-08-25 14:03:32 +0800

    提交

--- src/main/java/dy/network/hundred/HundredApplication.java
+++ src/main/java/dy/network/hundred/HundredApplication.java
@@ -14,19 +14,6 @@ public class HundredApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HundredApplication.class, args);
-
-        log.info("-----------------------------------------------------------------------");
-        log.info("       *\t\t\t\t\t\t\t好好工作");
-        log.info("       **\t\t\t\t\t\t\t好好生活");
-        log.info("*       ***");
-        log.info("**       ****");
-        log.info("******************\t\t\t\t\t祝小朋友节日快乐");
-        log.info("**       ****");
-        log.info("*       ***");
-        log.info("       **\t\t\t\t\t\t\t祝你爱的家人永远陪在你的身边");
-        log.info("       *\t\t\t\t\t\t\t希望兄弟情可以维持的时间长一点");
-        log.info("-----------------------------------------------------------------------");
-
     }
 
 }
